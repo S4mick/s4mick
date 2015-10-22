0ff87a312c5f905e9f7da21bb308639762305bcc
s4mick <s4mick@aol.com>
2015-10-22 15:12:23 +0200

    Create crashConsole.tcl
    Well just a small test xD

---
+++ crashCISCO/crashConsole.tcl
@@ -0,0 +1,25 @@
+use this script in tclsh of a cisco router to make it crash the console port...
+
+Router(conf)#:
+Router(conf)#:tclsh
+Router(tcl)#: While 1 {
+Router(tcl)#: puts "Your Router Just Got Pwnd by S4mick"
+Router(tcl)#: }
+
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+Your Router Just Got Pwnd by S4mick
+...
+..
+
+...
+
+Only wait to stop it Reboot the router
